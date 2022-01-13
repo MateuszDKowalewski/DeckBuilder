5db3928bd3ed8cafda708bd5cab61b392cc90a80
mkowalewski <aspoka1@gmail.com>
2022-01-12 22:07:47 -0800

    Generate points of hexagon

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from hexagonalPosition import HexagonalPosition
 
 
-hex_pos_1 = HexagonalPosition(0, 2, 0)
-hex_pos_2 = HexagonalPosition(-2, 0, -2)
+def prepare_hexagonal_net(side_length):
+    points = []
+    u = 0
+    v = 0
+    w = 0
+    step = 0
+    points.append(HexagonalPosition(0, 0, 0))
+    for i in range(0, side_length):
+        for j in range(0, 6):
+            for k in range(0, step):
+                points.append((HexagonalPosition(u, v, w)))
+                new_positon = {
+                    0: (u, v + 1, w),
+                    1: (u - 1, v, w),
+                    2: (u, v, w + 1),
+                    3: (u, v - 1, w),
+                    4: (u + 1, v, w),
+                    5: (u, v, w - 1)
+                }
+                (u, v, w) = new_positon.get(j)
+        step += 1
+    return points
 
-print("First point position {}".format(hex_pos_1.to_cartesian_position()))
-print("Second point position {}".format(hex_pos_2.to_cartesian_position()))
 
-print("Are positions equal: {}".format(hex_pos_1 == hex_pos_2))
-
-
+print(len(prepare_hexagonal_net(3)))
