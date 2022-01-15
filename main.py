f1a1e00f2a81474f2659822bcc861ee6da5066ec
mkowalewski <aspoka1@gmail.com>
2022-01-15 06:19:37 -0800

    Some refactor

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 import pygame
 import sys
 
-# hex_map = HexagonalMap(3)
-# print(hex_map)
-
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("First Game!")
-color = (255, 0, 0)
+RESOLUTION = (900, 500)
+WIN = pygame.display.set_mode(RESOLUTION)
+pygame.display.set_caption("Deck builder!")
 rect = pygame.Rect(30, 30, 60, 60)
 
 
 def main():
 
-    run = True
-    while run:
+    while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+            handle_quit_event(event)
 
         rect.x += 1
-        print(rect)
 
         WIN.fill((0, 0, 0))
-        pygame.draw.rect(WIN, color, rect)
+        pygame.draw.rect(WIN, (255, 0, 0), rect)
         pygame.display.flip()
+
+
+def handle_quit_event(event):
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
 
 
 if __name__ == "__main__":
